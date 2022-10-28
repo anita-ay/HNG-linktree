dd68ac71b849b6336efddc9cfa45a0f2e56a1387
anita-ay <ajalaanita690@gmail.com>
2022-10-28 12:32:05 +0100

    changes made to Id naming

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Product from "./Product";
 const items = [
   {
     href: "https://training.zuri.team/",
-    id: "btn_zuri",
+    id: "btn__zuri",
     content: "Zuri Team",
     desc: "",
   },
@@ -16,18 +16,21 @@ const items = [
   },
   {
     href: "https://books.zuri.team/python-for-beginners?ref_id=Ayomide-Ajala",
-    id: "book_python",
+    id: "book__python",
     content: "Python Books",
+    desc: "",
   },
   {
     href: "https://background.zuri.team",
     id: "pitch",
     content: "Run a quick background check to know who you are working with.",
+    desc: "",
   },
   {
     href: "https://books.zuri.team/design-rules",
-    id: "book_design",
+    id: "book__design",
     content: "Design Books",
+    desc: "",
   },
 ];
 function App() {
